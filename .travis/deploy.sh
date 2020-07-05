c839a7817910ab59312039bb8f53422b806b4f82
Vancoillie <jeremy.vancoillie@gmail.com>
2020-07-05 16:17:03 +0200

    test deploy rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-git config --global push.default matching
-git remote add deploy ssh://git@$HOST:$PORT$DEPLOY_DIR
-git push deploy master
-
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh $USER@$HOST -p $PORT <<EOF
-  cd $DEPLOY_DIR
-  make deploy
-EOF
+# rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/<dir> $USER@$HOST:$DEPLOY_DIR
+rsync --delete --exclude={.env.local,var/,public/pdf/} -Craqe  ssh $(pwd)/ ${PROD_USER}@${PROD_HOST}:${PROD_PATH}
+ssh ${PROD_USER}@${PROD_HOST} make --directory=${PROD_PATH} deploy@prod PHP=/opt/rh/rh-php71/root/bin/php
 
