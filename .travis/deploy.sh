ab2a80dfb066454ebb1aeb9f6805532d052d9f8c
Vancoillie <jeremy.vancoillie@gmail.com>
2020-07-05 16:40:43 +0200

    make call on deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,5 @@
 
 # rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/<dir> $USER@$HOST:$DEPLOY_DIR
 rsync --delete --exclude={.env.local,var/,public/pdf/} -Craqe  ssh $(pwd)/ ${PROD_USER}@${PROD_HOST}:${PROD_PATH}
-ssh ${PROD_USER}@${PROD_HOST} make --directory=${PROD_PATH} deploy@prod PHP=/opt/rh/rh-php71/root/bin/php
+ssh ${PROD_USER}@${PROD_HOST} make --directory=${PROD_PATH} deploy
 
