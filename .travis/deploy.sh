2b54d7a1b854385d16ce5e08e3b01085e40158b5
Vancoillie <jeremy.vancoillie@gmail.com>
2020-07-05 16:59:23 +0200

    add php path on make calls

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,5 @@
 
 # rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/<dir> $USER@$HOST:$DEPLOY_DIR
 rsync --delete --exclude={.env.local,var/,public/pdf/} -Craqe  ssh $(pwd)/ ${PROD_USER}@${PROD_HOST}:${PROD_PATH}
-ssh ${PROD_USER}@${PROD_HOST} make --directory=${PROD_PATH} deploy
+ssh ${PROD_USER}@${PROD_HOST} make --directory=${PROD_PATH} deploy PHP=/usr/bin/php7.3-cli
 
