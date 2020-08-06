dd99421e3eb9ddcd3d302003a7a0555972819195
Vancoillie <jeremy.vancoillie@gmail.com>
2020-08-06 16:04:26 +0200

    remove .htaccess

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/<dir> $USER@$HOST:$DEPLOY_DIR
-rsync --delete --exclude={.env.local,var/,public/pdf/, public/.htaccess} -Craqe  ssh $(pwd)/ ${PROD_USER}@${PROD_HOST}:${PROD_PATH}
+rsync --delete --exclude={.env.local,var/,public/pdf/} -Craqe  ssh $(pwd)/ ${PROD_USER}@${PROD_HOST}:${PROD_PATH}
 ssh ${PROD_USER}@${PROD_HOST} make --directory=${PROD_PATH} deploy PHP=/usr/bin/php7.3-cli
 
